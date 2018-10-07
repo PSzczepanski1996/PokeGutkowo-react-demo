45582fe8dc80f52555c65f3eac4551368119e7fd
p_szczepanski996 <pszczepanski@webtechnika.pl>
2018-10-07 22:16:06 +0200

    Add implementation of aboutme/discord link (routing)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './index.css';
+import { Switch, Route } from 'react-router-dom';
 
 function headerColor(playerTeam){
     const prefix = 'card-header '
@@ -50,19 +51,19 @@ class Players extends Component {
 
     render(){
         if(this.state.isLoading){
-            return <h7>Ładowanie</h7>;
+            return <h5>Ładowanie</h5>;
         }
-        return (
+        return(
             <div className='col-12'>
             <h1><b>Gracze</b></h1>
             {this.state.objects.map((item, index) => (
                 <div className='card mb-2' onClick={()=> this.handleClick(index)}>
                     <div className={headerColor(item.team)}>
-                        <h4>Drużyna: {item.team}</h4>
+                        <h3>Drużyna: {item.team}</h3>
                     </div>
-                        <h5>{item.nickname}</h5>
-                        <h7>Poziom: {item.level}</h7>
-                        {item.trainer_code ? <h7>Trainer code: {item.trainer_code}</h7> : null}
+                        <h4>{item.nickname}</h4>
+                        <h5>Poziom: {item.level}</h5>
+                        {item.trainer_code ? <h5>Trainer code: {item.trainer_code}</h5> : null}
                 </div>
             ))}
             </div>
@@ -70,4 +71,72 @@ class Players extends Component {
     }
 }
 
-export default Players;
+class About extends Component {
+    state = {
+        objects: [],
+        isLoading: false,
+    }
+
+    async componentDidMount(){
+        try {
+            this.setState({ isLoading: true });
+
+            const res = await fetch('http://127.0.0.1:8000/settings_api/');
+            const objects = await res.json();
+            this.setState({
+                objects: objects,
+                isLoading: false
+            });
+        } catch(e){
+            console.log(e);
+        }
+    }
+
+    render(){
+        if(this.state.isLoading){
+            return <h5>Ładowanie</h5>;
+        }
+        return(
+            <div>
+            {this.state.objects.map((item, index) => (
+                    <div>
+                        <b>O mnie:</b>
+                        <p>{item.owner_about}</p>
+                        <b>Link do Discorda:</b>
+                        <p><a href={item.discord}>Discord</a></p>
+                    </div>
+            ))}
+            </div>
+        );
+    }
+}
+
+
+class Website extends Component {
+     render(){
+        return(
+            <div>
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <a className="navbar-brand" href="#">PokeGutkowo demo</a>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="/">Gracze Gutkowa</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/aboutme">O mnie</a>
+                        </li>
+                    </ul>
+                </div>
+                </nav>
+
+                <Switch>
+                    <Route exact path='/' component={Players}></Route>
+                    <Route exact path='/aboutme' component={About}></Route>
+                </Switch>
+            </div>
+        );
+     }
+}
+
+export default Website;
--- pokegutkowo/views.py
+++ pokegutkowo/views.py
@@ -1,9 +1,14 @@
 # Create your views here.
 from rest_framework import generics
-from pokegutkowo.models import Players
-from pokegutkowo.serializers import TopPlayersSerializer
+from pokegutkowo.models import Players, Settings
+from pokegutkowo.serializers import PlayersSerializer, SettingsSerializer
 
 
 class PlayersView(generics.ListCreateAPIView):
     queryset = Players.objects.all()
-    serializer_class = TopPlayersSerializer
+    serializer_class = PlayersSerializer
+
+
+class SettingsView(generics.ListCreateAPIView):
+    queryset = Settings.objects.all()
+    serializer_class = SettingsSerializer
